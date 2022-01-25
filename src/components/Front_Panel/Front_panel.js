9a9d5f912880767c154a34cd9b5893653c65a382
riddhishwar <82893678+riddhishwar@users.noreply.github.com>
2022-01-25 15:58:23 +0530

    second commit

--- src/components/Front_Panel/Front_panel.js
+++ src/components/Front_Panel/Front_panel.js
@@ -1,11 +1,17 @@
 import { Outlet, Link, useParams } from 'react-router-dom';
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, onAuthStateChanged, sendEmailVerification } from "firebase/auth";
 import { app } from "../../firebase-config";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Swal from 'sweetalert2';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import './Front_panel.css';
 const auth = getAuth(app);
 function Front_panel() {
+    let email_verifys = false;
+    const [email_verify, isEmailVerify] = useState(email_verifys);
     let navigate = useNavigate();
     const call = () => {
         Swal.fire({
@@ -14,7 +20,7 @@ function Front_panel() {
             title: 'Successfully Logout',
             showConfirmButton: false,
             timer: 1500
-          })
+        })
         navigate("/login");
     }
     const logout = () => {
@@ -28,12 +34,20 @@ function Front_panel() {
             activeDivs.classList.add('show');
             actDivs.classList.remove('show');
             actDivs.classList.add('hidden');
+            //isEmailVerify(true);
             signOut(auth);
             console.log("Signout");
             call();
         } catch (error) {
             console.log("Error");
         }
+    }
+    function func() {
+        sendEmailVerification(auth.currentUser).then(() => {
+            console.log('verification email sent');
+        }).catch(error => {
+            console.log(error);
+        })
     }
     return (
         <div id="upperdiv">
@@ -45,6 +59,20 @@ function Front_panel() {
             <div className="bt2" style={{ position: "absolute", top: "10px" }}><button className="button-27" role="button"><Link to={`/Register`}>Signup</Link></button></div>
             <div className="bt3 hidden" style={{ position: "absolute", top: "10px" }} onClick={logout}><button className="button-27" role="button">Logout</button></div>
             <Outlet />
+            <div style={{ position: "fixed", top: "5rem", color: "white", fontSize: "1.5rem" }}>
+                {(() => {
+                    onAuthStateChanged(auth, (user) => {
+                        if (user) {
+                          const uid = user.uid;
+                          if(!user.emailVerified){isEmailVerify(true)}
+                        } else {
+                            isEmailVerify(false);
+                        }
+                      });
+                    if (email_verify) { return <div><div>Your Email has not been Verified . Please click the button to verify</div><div><Button variant="primary" onClick={() => func()}>Primary</Button></div></div> }
+                }
+                )()}
+            </div>
         </div>
     );
 };
--- src/components/Front_Panel/Register_form.js
+++ src/components/Front_Panel/Register_form.js
@@ -13,18 +13,27 @@ function Register_form() {
     const [username, setusername] = useState("");
     const [email, setemail] = useState("");
     const [password, setPassword] = useState("");
+    const [phonenumber,setphonenumber]=useState();
     const SignupSubmit = async (req, res) => {
         try {
             const data = {
                 firstname: firstname,
                 lastname: lastname,
-                username: username
+                username: username,
+                phonenumber:phonenumber
             };
+            let arr=[];
+            console.log(phonenumber.length);
+            if(firstname=="" || phonenumber.length!=10){
+                if(firstname=="") throw new Error("Check your UserName");
+                if(phonenumber.length!=10) throw new Error("Check your PhoneNumber");
+            }
             console.log(data);
-            addDoc(corel, data);
             const auth = getAuth(app);
+            console.log(getAuth(app));
             createUserWithEmailAndPassword(auth, email, password)
                 .then((userCredential) => {
+                    addDoc(corel, data);
                     console.log(userCredential);
                     Swal.fire(
                         'Good job!',
@@ -57,7 +66,7 @@ function Register_form() {
                 <Row className="g-2">
                     <Col md>
                         <FloatingLabel controlId="floatingInputGrid" label="Fullname">
-                            <Form.Control type="text" placeholder="_" onChange={e => setfirstname(e.target.value)} value={firstname} />
+                            <Form.Control type="text" placeholder="_" onChange={e => setfirstname(e.target.value)} value={firstname} required/>
                         </FloatingLabel>
                     </Col>
                     <Col md>
@@ -66,21 +75,27 @@ function Register_form() {
                         </FloatingLabel>
                     </Col>
                 </Row>
-                <Row className="g-2" style={{ marginTop: "1.5rem" }}>
+                //phonenumber and username
+                <Row className="g-2">
                     <Col md>
-                        <FloatingLabel controlId="floatingInputGrid" label="Username">
-                            <Form.Control type="text" placeholder="_" onChange={e => setusername(e.target.value)} value={username} />
+                        <FloatingLabel controlId="floatingInputGrid" label="username">
+                            <Form.Control type="text" placeholder="username" onChange={e => setusername(e.target.value)} value={username} required/>
+                        </FloatingLabel>
+                    </Col>
+                    <Col md>
+                        <FloatingLabel controlId="floatingInputGrid" label="Number">
+                            <Form.Control type="number" placeholder="PhoneNumber" onChange={e => setphonenumber(e.target.value)} value={phonenumber} required/>
                         </FloatingLabel>
                     </Col>
                 </Row>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label style={{ width: "100%", textAlign: "center", color: "#00ffff", fontFamily: 'Merriweather', fontSize: "1rem" }}>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={e => setemail(e.target.value)} value={email} />
+                    <Form.Control type="email" placeholder="Enter email" onChange={e => setemail(e.target.value)} value={email} required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label style={{ width: "100%", textAlign: "center", color: "#00ffff", fontFamily: 'Merriweather', fontSize: "1rem" }}>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} value={password} />
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} value={password} required/>
                 </Form.Group>
                 <div>
                     <div className="bt2" style={{ textAlign: "center" }}><button className="button-27" role="button" style={{ width: "6rem" }} onClick={SignupSubmit}>Signup</button></div>
